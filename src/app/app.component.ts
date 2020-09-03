937ddcc038c23ffa5d3c99080bd15f6b71c09f17
DawnLuna <xjy.pub@gmail.com>
2020-09-03 00:27:20 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'forum-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'Restful-Backend-Angular-Forum';
+}
